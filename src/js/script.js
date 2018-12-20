8aadc863916c6b42a845161e38d6758c3690e009
leadmrn <leadeamorin.ps@gmail.com>
2018-12-20 14:25:22 +0100

    inte

--- src/js/script.js
+++ src/js/script.js
@@ -75,123 +75,130 @@ let tabRecupElements = [];
 let tabAnswerUser = [];
 let obstacleSize = 100;
 let speed = 100;
+let perso1;
+let perso2 = document.getElementById('perso2');
 
-oxo.inputs.listenKey('enter', function() {
-  if (oxo.screens.getCurrentScreen !== 'game') {
-      oxo.inputs.cancelKeyListener('enter');
-      oxo.screens.loadScreen('game',function(){
-      oxo.player.setScore(300);
-      goku = document.querySelector(".game__square");      
-      setInterval(timer , 1000);
-      function timer() {
-        if(oxo.player.getScore() == 0 ){
-          oxo.screens.loadScreen('end', function() {
-            oxo.player.setScore(0);//bastien 
-            clearInterval(intervalWall);
-          });
-        }else{
-          oxo.player.removeFromScore(1);
-        };
-      };
-      let wallNumber = 1;
-      intervalWall = setInterval(addWall, speed*20);
-      
-      function addWall() {
-
-        let blockTab = [];
-        let numbersTab = [0, 1, 2, 3, 4, 5, 6, 7];
-        let bonusBlock = oxo.utils.getRandomNumber(0, 7);
-
-        let bonus;
-        let tabDirection = ['up', 'down', 'left', 'right'];
-        let arrowrand = tabDirection[oxo.utils.getRandomNumber(0, 3)];
-        let none = 'none'
-        let tabDisplay = [arrowrand , arrowrand , none]
-        
-        let wall = oxo.elements.createElement({
-          type: 'div',
-          class: 'game__wall game__wall--' + wallNumber,
-          styles: {
-            transform: 'translate(1100px, 0)',
-          },
-          appendTo: 'body'
-        });
-        oxo.elements.onLeaveScreenOnce(wall, function() {
-          wall.remove();
-        });
-        //console.log(wallumber);
-        for(let i = 0; i < 8; i++){
-          let tempIndex = oxo.utils.getRandomNumber(0, numbersTab.length-1);
-          blockTab[i] = numbersTab[tempIndex];
-          numbersTab.splice(tempIndex, 1);
-          if(i === bonusBlock){
-            bonus = oxo.elements.createElement({
-              class: 'game__bonus game__bonus--' + tabDisplay[oxo.utils.getRandomNumber(0, 2)],
-              appendTo: '.game__wall--' + wallNumber,
-              styles: {
-                transform: 'translate(0px, ' + blockTab[i] * obstacleSize +'px)'
-              },
-            });
-            oxo.elements.onCollisionWithElementOnce(goku, bonus, function() {
-              bonus.remove();
-
-              let upValue;
-              let downValue;
-              let leftValue;
-              let rightValue;
-              
-              // console.log(bonus.className)
-              if(bonus.className === 'game__bonus game__bonus--up'){
-                upValue = 'up';
-                if(tabRecupElements.length < 7){
-                  tabRecupElements.push(upValue);
-                  console.log(tabRecupElements);
-                  checkAnswer();
-                };
-              };
-              if(bonus.className == 'game__bonus game__bonus--down'){
-                downValue = 'down';
-                if(tabRecupElements.length < 7){
-                  tabRecupElements.push(downValue);
-                  console.log(tabRecupElements);
-                  checkAnswer();
-                };
-              };
-              if(bonus.className == 'game__bonus game__bonus--left'){
-                leftValue = 'left';
-                if(tabRecupElements.length < 7){
-                  tabRecupElements.push(leftValue);
-                  console.log(tabRecupElements);
-                  checkAnswer();
-                };
-              };
-              if(bonus.className == 'game__bonus game__bonus--right'){
-                rightValue = 'right';
-                if(tabRecupElements.length < 7){
-                  tabRecupElements.push(rightValue);
-                  console.log(tabRecupElements);
-                  checkAnswer();
-                };
-              };
-            });
-          }else{
-            let obstacle = oxo.elements.createElement({
-              class: 'game__obstacle',
-              appendTo: '.game__wall--' + wallNumber,
-              styles: {
-                transform: 'translate(0px, ' + blockTab[i] * obstacleSize +'px)'
-              },
-            });
-            oxo.elements.onCollisionWithElementOnce(goku, obstacle , function() {
-              oxo.screens.loadScreen('game', function() {
-                goku = document.querySelector(".game__square");
-                tabRecupElements = [];
+oxo.screens.loadScreen('home', function() {
+  perso1 = document.getElementById('perso1');
+  perso1.addEventListener('click', function() {
+    oxo.inputs.listenKey('enter', function() {
+      if (oxo.screens.getCurrentScreen !== 'game') {
+          oxo.inputs.cancelKeyListener('enter');
+          oxo.screens.loadScreen('game',function(){
+          oxo.player.setScore(300);
+          goku = document.querySelector(".game__square");      
+          setInterval(timer , 1000);
+          function timer() {
+            if(oxo.player.getScore() == 0 ){
+              oxo.screens.loadScreen('end', function() {
+                oxo.player.setScore(0);//bastien 
+                clearInterval(intervalWall);
               });
-            });
+            }else{
+              oxo.player.removeFromScore(1);
+            };
           };
-        };
-        wallNumber++;
+          let wallNumber = 1;
+          intervalWall = setInterval(addWall, speed*20);
+          
+          function addWall() {
+    
+            let blockTab = [];
+            let numbersTab = [0, 1, 2, 3, 4, 5, 6, 7];
+            let bonusBlock = oxo.utils.getRandomNumber(0, 7);
+    
+            let bonus;
+            let tabDirection = ['up', 'down', 'left', 'right'];
+            let arrowrand = tabDirection[oxo.utils.getRandomNumber(0, 3)];
+            let none = 'none'
+            let tabDisplay = [arrowrand , arrowrand , none]
+            
+            let wall = oxo.elements.createElement({
+              type: 'div',
+              class: 'game__wall game__wall--' + wallNumber,
+              styles: {
+                transform: 'translate(1100px, 0)',
+              },
+              appendTo: 'body'
+            });
+            oxo.elements.onLeaveScreenOnce(wall, function() {
+              wall.remove();
+            });
+            //console.log(wallumber);
+            for(let i = 0; i < 8; i++){
+              let tempIndex = oxo.utils.getRandomNumber(0, numbersTab.length-1);
+              blockTab[i] = numbersTab[tempIndex];
+              numbersTab.splice(tempIndex, 1);
+              if(i === bonusBlock){
+                bonus = oxo.elements.createElement({
+                  class: 'game__bonus game__bonus--' + tabDisplay[oxo.utils.getRandomNumber(0, 2)],
+                  appendTo: '.game__wall--' + wallNumber,
+                  styles: {
+                    transform: 'translate(0px, ' + blockTab[i] * obstacleSize +'px)'
+                  },
+                });
+                oxo.elements.onCollisionWithElementOnce(goku, bonus, function() {
+                  bonus.remove();
+    
+                  let upValue;
+                  let downValue;
+                  let leftValue;
+                  let rightValue;
+                  
+                  // console.log(bonus.className)
+                  if(bonus.className === 'game__bonus game__bonus--up'){
+                    upValue = 'up';
+                    if(tabRecupElements.length < 7){
+                      tabRecupElements.push(upValue);
+                      console.log(tabRecupElements);
+                      checkAnswer();
+                    };
+                  };
+                  if(bonus.className == 'game__bonus game__bonus--down'){
+                    downValue = 'down';
+                    if(tabRecupElements.length < 7){
+                      tabRecupElements.push(downValue);
+                      console.log(tabRecupElements);
+                      checkAnswer();
+                    };
+                  };
+                  if(bonus.className == 'game__bonus game__bonus--left'){
+                    leftValue = 'left';
+                    if(tabRecupElements.length < 7){
+                      tabRecupElements.push(leftValue);
+                      console.log(tabRecupElements);
+                      checkAnswer();
+                    };
+                  };
+                  if(bonus.className == 'game__bonus game__bonus--right'){
+                    rightValue = 'right';
+                    if(tabRecupElements.length < 7){
+                      tabRecupElements.push(rightValue);
+                      console.log(tabRecupElements);
+                      checkAnswer();
+                    };
+                  };
+                });
+              }else{
+                let obstacle = oxo.elements.createElement({
+                  class: 'game__obstacle',
+                  appendTo: '.game__wall--' + wallNumber,
+                  styles: {
+                    transform: 'translate(0px, ' + blockTab[i] * obstacleSize +'px)'
+                  },
+                });
+                oxo.elements.onCollisionWithElementOnce(goku, obstacle , function() {
+                  oxo.screens.loadScreen('game', function() {
+                    goku = document.querySelector(".game__square");
+                    tabRecupElements = [];
+                  });
+                });
+              };
+            };
+            wallNumber++;
+          };
+        }); 
       };
-    }); 
-  };
-});
+    });
+  })
+})
